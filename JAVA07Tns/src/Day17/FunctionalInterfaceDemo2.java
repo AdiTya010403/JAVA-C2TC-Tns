f4ceb80cac1edc2c6f21af8db11a66494706ba19
AdiTya010403 <adityavthakare01@gmail.com>
2024-06-18 22:45:26 +0530

    Day 17

--- JAVA07Tns/src/Day17/FunctionalInterfaceDemo2.java
+++ JAVA07Tns/src/Day17/FunctionalInterfaceDemo2.java
@@ -11,9 +11,35 @@ public class FunctionalInterfaceDemo2 {
 		System.out.println(greet.sayHelloString("Students!"));
 		
 		greet = (s)-> "hi "+s+" How Are You?";
-		System.out.println(greet.sayHelloString("Students!"));		
-	};
+		System.out.println(greet.sayHelloString("Students!"));	
+		
+		System.out.println("\n\nUsing Implements");
+		
+		greet = new Greet1();
+		System.out.println("\n"+greet.sayHelloString("Aditya !"));
+
+		greet = new Greet2();
+		System.out.println("\n"+greet.sayHelloString("Shreya !"));
+	}
+
+}
+
+class Greet1 implements GreetInterface{
+
+	@Override
+	public String sayHelloString(String name) {
+		
+		return "Hello "+name;
+	}
 	
+}
 
+class Greet2 implements GreetInterface{
 
+	@Override
+	public String sayHelloString(String name) {
+		
+		return "Hi "+name+" How are you?";
+	}
+	
 }
